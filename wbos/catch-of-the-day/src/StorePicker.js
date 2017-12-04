74add0ced4d3a8684879d0c9614b7678a083b00f
Mitchell Vega <neuralnet@gmail.com>
2017-12-04 12:30:59 -0800

    added event listeners

--- wbos/catch-of-the-day/src/StorePicker.js
+++ wbos/catch-of-the-day/src/StorePicker.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { getFunName } from './helpers';
 
 class StorePicker extends React.Component {
+	goToStore(event) {
+		event.preventDefault();
+		console.log(this.storeInput.value)
+		//move url
+	}
 	render(){
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 			{ /* this is a comment */}
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" />
+	<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
 				<img src="" alt="" />
-				<button class="submit">Visit Store</button>
+				<button type="submit">Visit Store</button>
 			</form> 
 			
 		)
