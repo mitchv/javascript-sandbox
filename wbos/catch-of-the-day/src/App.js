b6b64ba10366ed20a7edbf95d7b7ee52cc3084c7
Mitchell Vega <neuralnet@gmail.com>
2017-12-05 15:53:55 -0800

    carousel files

--- wbos/catch-of-the-day/src/App.js
+++ wbos/catch-of-the-day/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from './sample-fishes';
 
 class App extends React.Component {
 	constructor () {
@@ -18,18 +20,30 @@ class App extends React.Component {
 		const fishes = {...this.state.fishes};
 		// add in our new fish
 		const timestamp = Date.now();
-		fishes["fish-${timestamp)"] = fish;
+		fishes[`fish-${timestamp}`] = fish;
 		// set state
 		this.setState({fishes});
 	}
-	
+			
+	loadSamples = () => {
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+		
 	render(){
 		return (
 			<div className="catch-of-the-day">
-				<div>
-					<Header tagline="Fresh Seafood Market" />
+				<div className="menu">
+					<Header tagline='Fresh Seafood Market' />
+						<ul className="list-of-fishes">
+						{
+							Object.keys(this.state.fishes)
+							.map(key => <Fish key={key} details={this.state.fishes[key]} />)
+						}
+						</ul>
 					<Order />
-					<Inventory addFish={this.addFish} />
+					<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 				</div>
 			</div>
 		)
---
+++ wbos/catch-of-the-day/src/Fish.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Fish extends React.Component {
+	render () {
+			const {details}  = this.props;
+			return (
+				<li className="menu-fish">
+					<img src="{this.props.details.name}" alt="{this.props.details.name}" />
+					<h3 className="fish-name">
+						{details.name}
+						<span className="price">{details.price}</span>
+					</h3>
+					<p>{details.desc}</p>
+					<button>Add To Order</button>
+				</li>
+				);
+		}
+}
+
+export default Fish;
