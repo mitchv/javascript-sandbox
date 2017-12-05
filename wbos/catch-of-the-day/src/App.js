a14ab1e0ed4c5af04c3b1df95d0f6468f8d707c3
Mitchell Vega <neuralnet@gmail.com>
2017-12-04 17:01:42 -0800

    added event listeners

--- wbos/catch-of-the-day/src/App.js
+++ wbos/catch-of-the-day/src/App.js
@@ -6,18 +6,30 @@ import Inventory from './Inventory';
 class App extends React.Component {
 	constructor () {
 		super();
+		this.addFish = this.addFish.bind(this);
 		this.state = {
 			fishes: {},
 			order: {},
 		};
 	}
+	
+	addFish(fish) {
+		// update state
+		const fishes = {...this.state.fishes};
+		// add in our new fish
+		const timestamp = Date.now();
+		fishes["fish-${timestamp)"] = fish;
+		// set state
+		this.setState({fishes});
+	}
+	
 	render(){
 		return (
 			<div className="catch-of-the-day">
 				<div>
 					<Header tagline="Fresh Seafood Market" />
 					<Order />
-					<Inventory />
+					<Inventory addFish={this.addFish} />
 				</div>
 			</div>
 		)
