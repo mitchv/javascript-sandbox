5691fe67137eae6a847c271407164af71fe5a139
Mitchell Vega <neuralnet@gmail.com>
2017-12-07 13:49:37 -0800

    fixed JS errors

--- wbos/catch-of-the-day/src/Fish.js
+++ wbos/catch-of-the-day/src/Fish.js
@@ -5,10 +5,10 @@ class Fish extends React.Component {
 			const {details}  = this.props;
 			return (
 				<li className="menu-fish">
-					<img src="{this.props.details.name}" alt="{this.props.details.name}" />
+					<img src={this.props.details.image} alt={this.props.details.name} />
 					<h3 className="fish-name">
-						{details.name}
-						<span className="price">{details.price}</span>
+						{this.props.details.name}
+						<span className="price">{this.props.details.price}</span>
 					</h3>
 					<p>{details.desc}</p>
 					<button>Add To Order</button>
