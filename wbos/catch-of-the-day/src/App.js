eff83163039d349a6999bf7d07bf3027179d70eb
Mitchell Vega <neuralnet@gmail.com>
2017-12-08 13:17:28 -0800

    a few small js changes

--- wbos/catch-of-the-day/src/App.js
+++ wbos/catch-of-the-day/src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
 		super();
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
-		this.addOrder = this.addOrder.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 		this.state = {
 			fishes: {},
 			order: {},
@@ -36,7 +36,7 @@ class App extends React.Component {
 	addToOrder(key) {
 		const order = {...this.state.order};
 		order[key] = order[key] + 1 || 1;
-		this.setSTate({order});
+		this.setState({order});
 	}
 		
 	render(){
@@ -47,7 +47,7 @@ class App extends React.Component {
 						<ul className="list-of-fishes">
 						{
 							Object.keys(this.state.fishes)
-							.map(key => <Fish key={key} details={this.state.fishes[key]} />)
+							.map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
 						}
 						</ul>
 					<Order />
